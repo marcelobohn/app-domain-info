8547bcdf9709a21c4064507771dc3065b7d81c12
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-06-26 16:46:45 -0300

    inclusão do comportamento de ao digitar e precionar tecla enter altera a tela

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      domain: '',
+      registerA: '',
+      namesSevers: '',
+      whois: '',
+      hostName: ''
+    };
+  }
+
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      this.setState({ domain: event.target.value });
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Consulta de dados de dominio</h2>
-          <input type="text" size="50" placeholder="digite o endereço"/>
+          <input type="text" size="50" placeholder="digite o endereço"
+            onKeyPress={this.handleKeyPress}/>
         </div>
         <p className="App-intro">
           <div>
             <h3>Domínio</h3>
-            <p></p>
+            <p>{ this.state.domain }</p>
           </div>
           <div>
             <h3>Registro A</h3>
-            <p></p>
+            <p>{ this.state.registerA.address }</p>
           </div>
         </p>
       </div>
